96c202233cf7f46cc7fb6ae8682d5aad681a6314
dujarfa <dujar.coding@gmail.com>
2021-10-23 12:09:35 +0800

    redux infra + eg

--- packages/interface/src/app/App.tsx
+++ packages/interface/src/app/App.tsx
@@ -5,14 +5,19 @@ import logo from './logo.svg';
 import greeterAbi from "@solidroid/core/deployed/unknown/greeter.json";
 import { Greeter } from "@solidroid/core/typechain/Greeter"
 import { ethers } from "ethers";
-
+import { useAppSelector, useAppDispatch } from "../hooks";
+import { setIsDark } from "../slices";
 let greeter =  (new ethers.Contract(greeterAbi.address,greeterAbi.abi)) as Greeter
 
 
 
 function App() {
+
+  const theme = useAppSelector(state => state.theme);
+  const dispatch = useAppDispatch();
   return (
     <div className="container bg-blue-300">
+      <button onClick={()=>{dispatch(setIsDark(!theme))}}>{theme+""}</button>
       SOLIDROID
       
     </div>
