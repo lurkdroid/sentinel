06c8e4064cc58f50c40e3e61aa57266b2cc4c558
dujarfa <dujar.coding@gmail.com>
2021-10-25 18:42:10 +0800

    up, issue with typechain however it might work for u

---
+++ packages/core/test/TestSoliDroidManager.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { BigNumber } from "@ethersproject/bignumber";
+import { ethers } from "hardhat";
+import { SoliDroidManager__factory } from '../typechain/factories/SoliDroidManager__factory';
+import { SoliDroidManager } from '../typechain/SoliDroidManager';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+describe("SoliDroidManaget", function () {
+
+    let accounts: SignerWithAddress[];
+    let manager: SoliDroidManager;
+
+    beforeEach(async function () {
+        accounts = await ethers.getSigners();
+        manager = await new SoliDroidManager__factory(accounts[0]).deploy();
+    });
+
+    it("Should test BotInstance update", async function () {
+
+        await expect(manager.updateBot(
+            "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+            BigNumber.from("100"),
+            BigNumber.from("500"),
+            true
+        )).to.emit(SoliDroidManager, "BotCreated")
+    });
+});
