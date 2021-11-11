d7bb05ba838d72121cfd4b1da2058e73c6f168b9
quickfox15 <quickfox15@gmail.com>
2021-11-11 09:17:45 -0500

    add internal json readonly database

--- packages/interface/src/utils/data/sdDatabase.ts
+++ packages/interface/src/utils/data/sdDatabase.ts
@@ -1,6 +1,24 @@
 const _addresses = require('./solidroid-address.json');
 
 
+export declare interface DbToken {
+    id:number;
+    name: string;
+    address: string;
+    img_32: string;
+    isQuote: boolean;
+    decimals: number
+}
+
 export function managerAddress(network:string):string{
     return _addresses[network].manager.address;
 }
+
+export function getDBToken(network:string,address:string):string{
+    return _addresses[network].manager.address;
+}
+
+export function getDBTokens(network:string):Array<DbToken>{
+    return _addresses[network].tokens;
+}
+
