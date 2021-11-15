351411faa3de39c22fb77e8eed3d61e7129a5741
dujarfa <dujar.coding@gmail.com>
2021-11-15 23:10:23 +0800

    updated writing deployment addresses to react interface

---
+++ packages/core/scripts/writeAddressesToInterface.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
+import { importAll } from '../deployed';
+
+const writeAddresses = async () => {
+    const networks = ['kovan', 'matic'] as const;
+    const addresses: any = {};
+    await Promise.all(networks.map(async (network) => {
+        const networkAddress = await importAll(network);
+        addresses[network] = networkAddress;
+    }));
+
+    const fileToInterface = path.resolve(__dirname, '../../interface/src/utils/addresses.json');
+
+    fs.writeFileSync(fileToInterface, JSON.stringify(addresses));
+}
+
+
+writeAddresses().catch(e => {
+    console.log("error writing the addresses", e)
+})
+
