f4c657869e371fd744f3e300b39f395bbb09cb31
quickfox15 <quickfox15@gmail.com>
2021-11-21 09:24:12 -0500

    date format

--- packages/interface/src/utils/FormatUtil.ts
+++ packages/interface/src/utils/FormatUtil.ts
@@ -2,14 +2,22 @@
 import bigDecimal from "js-big-decimal";
 
 export function toDateTimeStr(timestamp: string):string {
-        let date = new Date((+timestamp)*1000);
+        let dt = new Date((+timestamp)*1000);
 
-        return date.getDate().toString()+
-        "/"+(date.getMonth()+1)+
-        "/"+(date.getFullYear()-2000 )+
-        " "+date.getHours()+
-        ":"+date.getMinutes()+
-        ":"+date.getSeconds();    
+        return `${
+            (dt.getMonth()+1).toString().padStart(2, '0')}-${
+            dt.getDate().toString().padStart(2, '0')}-${
+            dt.getFullYear().toString().padStart(4, '0')} ${
+            dt.getHours().toString().padStart(2, '0')}:${
+            dt.getMinutes().toString().padStart(2, '0')}:${
+            dt.getSeconds().toString().padStart(2, '0')}`
+        
+        // return date.getDate().toString()+
+        // "/"+(date.getMonth()+1)+
+        // "/"+(date.getFullYear()-2000 )+
+        // " "+date.getHours()+
+        // ":"+date.getMinutes()+
+        // ":"+date.getSeconds();    
     }
 
 export function formatAmount(amount:string|number, precision:number){
