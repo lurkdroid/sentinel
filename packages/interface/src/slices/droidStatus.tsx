099d242bc5ca54106b7cfc597587a57277592a80
quickfox15 <quickfox15@gmail.com>
2021-11-25 22:20:01 -0500

    validate initial amount

--- packages/interface/src/slices/droidStatus.tsx
+++ packages/interface/src/slices/droidStatus.tsx
@@ -256,6 +256,7 @@ export function calcPrice(store: RootState, lastAmount: string): string {
   try {
     if (
       droid.position?.initialAmountIn === undefined ||
+      droid.position?.initialAmountIn === "0" ||
       droid.config?.quoteAsset === undefined ||
       lastAmount === undefined ||
       lastAmount === "0"
